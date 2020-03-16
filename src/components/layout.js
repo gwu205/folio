bfda3fd2ba8d034c80dae591fb9d30be248fcb4e
Geoffrey Wu <38195698+gwu205@users.noreply.github.com>
2020-03-17 06:28:52 +0900

    Added new case study Stylehint

--- src/components/layout.js
+++ src/components/layout.js
@@ -95,7 +95,7 @@ class Layout extends React.Component {
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
-          maxWidth: rhythm(32),
+          maxWidth: rhythm(28),
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
