ce37cb2ba5cff624b0eca3e2b1520ebc389f3e75
Geoffrey Wu <geoffrey@crowdcast.jp>
2022-03-21 18:23:47 +0900

    Add dark mode toggle

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,21 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Link, StaticQuery, graphql } from "gatsby"
 import { isMobile } from "react-device-detect"
-import { StaticImage } from "gatsby-plugin-image"
-import { Mail, MailOpen } from "react-ionicons"
-import { container, logo, navigation, active, mail } from "./header.module.scss"
+import Logo from "./logo"
+import { Mail, MailOpen, Sunny } from "react-ionicons"
+import {
+  container,
+  navigation,
+  active,
+  mail,
+  toggle,
+} from "./header.module.scss"
 
 const Header = ({ location }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isWorks =
     location.pathname === rootPath || location.pathname.indexOf("/works") > -1
+
   const [isHovering, setIsHovering] = useState(false)
   const handleMouseOver = () => {
     setIsHovering(true)
@@ -16,6 +23,30 @@ const Header = ({ location }) => {
   const handleMouseOut = () => {
     setIsHovering(false)
   }
+
+  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)")
+    .matches
+  const [darkMode, setDarkMode] = useState(false)
+
+  useEffect(() => {
+    const currentTheme = JSON.parse(localStorage.getItem("DARK_MODE"))
+    if (currentTheme === null) {
+      setDarkMode(prefersDarkScheme)
+    } else if (currentTheme) {
+      setDarkMode(true)
+    } else {
+      setDarkMode(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.setAttribute("data-theme", "dark")
+    } else {
+      document.documentElement.removeAttribute("data-theme")
+    }
+    localStorage.setItem("DARK_MODE", darkMode)
+  }, [darkMode])
 
   return (
     <StaticQuery
@@ -33,14 +64,7 @@ const Header = ({ location }) => {
       render={data => (
         <header className={container}>
           <Link to={`/`}>
-            <StaticImage
-              src="../../content/assets/gwlogo.png"
-              alt="Logo"
-              width={160}
-              height={160}
-              placeholder="none"
-              className={logo}
-            />
+            <Logo />
           </Link>
           <nav className={navigation}>
             <Link className={isWorks && active} to={rootPath}>
@@ -73,6 +97,13 @@ const Header = ({ location }) => {
               ) : (
                 <Mail width="16px" height="16px" />
               )}
+            </a>
+            <a
+              className={toggle}
+              onClick={() => setDarkMode(!darkMode)}
+              title={darkMode ? "Let there be light!" : "Dim the lights!"}
+            >
+              <Sunny width="16px" height="16px" />
             </a>
           </nav>
         </header>
